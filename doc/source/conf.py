24b32a2c680a0b47229af5a92ea42a7040f573d4
rogerluo <coolww@outlook.com>
2020-08-19 16:29:27 +0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('..\..\.'))
+sys.path.append(os.path.abspath('../../.'))
 from recommonmark.transform import AutoStructify
 
 #%% -- Project information -----------------------------------------------------
