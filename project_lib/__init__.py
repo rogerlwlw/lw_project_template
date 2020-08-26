c30d9f6aad59f8be7f7a5745aca73fbf3ca74287
rogerluo <coolww@outlook.com>
2020-08-26 14:20:05 +0800

    update

--- project_lib/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Aug 13 14:27:10 2020
-
-@author: rogerluo
-"""
-
-
-__version__ = '0.0.1'
--- project_lib/dataset/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-dataset package to retrieve demo data
-
-Created on Thu Aug 13 14:27:10 2020
-
-@author: rogerluo
-
-"""
